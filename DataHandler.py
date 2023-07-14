7381553d04bc7cd73aed47638be25217f8a45b70
JannesAccal <jannes.de.cock@gmail.com>
2023-07-14 10:55:31 +0200

    initial commit

--- DataHandler.py
+++ DataHandler.py
@@ -1,22 +1,36 @@
 import xlwings as xw
 import pandas as pd
-from tkinter import Tk, Label, Text, Button
+import tkinter as tk
+from tkinter import filedialog, Text, Button
+
+
+def select_file():
+    # Open a file selection dialog
+    root.filename = filedialog.askopenfilename(initialdir="/", title="Select Excel File",
+                                               filetypes=(("Excel Files", "*.xlsx"), ("All Files", "*.*")))
+    file_entry.insert(tk.END, root.filename)
+    print(root.filename)
 
 
 def fetch_data():
-    # Open the Excel file to fetch data from
-    source_file_path = "BECRIS.xlsx"  # Specify the source file name
-    wb = xw.Book(source_file_path)
+    # Get the selected file path
+    file_path = file_entry.get()
+    print(file_path)
 
-    # Get the "04-2022" sheet of the workbook
-    source_sheet = wb.sheets['04-2022']
+    # Open the Excel file
+    print("Opening file...")
+    wb = xw.books.open(file_path)
+    print("File successfully opened")
+
+    # Get the "05-2022" sheet of the workbook
+    source_sheet = wb.sheets['05-2022']
 
     # Find the last row with data in column A of the source sheet
     last_row = source_sheet.range("A" + str(source_sheet.cells.last_cell.row)).end("up").row
 
     # Read the data from the source sheet into a DataFrame
-    header_range = source_sheet.range("A1").expand("right")
-    data_range = source_sheet.range("A2").expand("down").resize(last_row - 1, header_range.columns.count)
+    header_range = source_sheet.range("A2:AK2")
+    data_range = source_sheet.range("A3").expand("down").resize(last_row - 1, header_range.columns.count)
 
     columns = header_range.value
     data = data_range.value
@@ -24,42 +38,32 @@ def fetch_data():
     # Create an empty list to store the filtered data rows
     filtered_data = []
 
-    # Iterate over the data rows and filter based on the condition for 'schAcode' column
+    # Iterate over the data rows and filter
     for row in data:
-        if str(row[1]).startswith(('11', '12')):
+        if str(row[columns.index("sch A acc")]).startswith(('11', '12')) and row[columns.index("ACCT NO.")] != {None,0} and str(row[columns.index("CN")]) != {None,"ALL"}:
             filtered_data.append(row)
 
     # Create a DataFrame from the filtered data
     df = pd.DataFrame(filtered_data, columns=columns)
 
     # Convert the values in the 'schAcode' column to strings
-    df['schAcode'] = df['schAcode'].astype(str)
-
-    # Get the "Data" sheet of the workbook
-    target_sheet = wb.sheets['Data']
-
-    # Find the last row with data in column A of the target sheet
-    last_row = target_sheet.range("A" + str(target_sheet.cells.last_cell.row)).end("up").row
-
-    # Write the header row to the "Data" sheet
-    header_range = target_sheet.range("A1").expand("right")
-    header_range.value = columns
-
-    # Write the data to the "Data" sheet starting from the first empty row
-    row_offset = last_row + 1
-    data_range = target_sheet.range((row_offset, 1), (row_offset + len(df), len(columns) - 1))
-    data_range.value = df.values
+    df['sch A acc'] = df['sch A acc'].astype(str)
 
     # Display the data in the GUI
     data_text.delete("1.0", "end")
     data_text.insert("1.0", df.to_string())
 
-# Create the GUI
-root = Tk()
+    print(df)
 
-# Label for data display
-data_label = Label(root, text="Data:")
-data_label.pack()
+# Create the GUI
+root = tk.Tk()
+
+# File selection button and entry
+select_button = Button(root, text="Select File", command=select_file)
+select_button.pack()
+
+file_entry = tk.Entry(root)
+file_entry.pack()
 
 # Text widget for displaying data
 data_text = Text(root)

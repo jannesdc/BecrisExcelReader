a2692d5b4067ab769c1abcf96df70b6a778b2f1b
JannesAccal <jannes.de.cock@gmail.com>
2023-07-14 14:12:43 +0200

    add: replace information in text box, instead of showing all data now shows a summary

--- DataHandler.py
+++ DataHandler.py
@@ -2,6 +2,7 @@ import xlwings as xw
 import pandas as pd
 import tkinter as tk
 from tkinter import filedialog, Text, Button
+import tabulate
 
 global dataframe
 global wb_to_use
@@ -70,14 +71,63 @@ def fetch_data():
     # Convert the values in the 'sch A acc' column to strings
     df['sch A acc'] = df['sch A acc'].astype(str)
 
-    # Display the data in the GUI
+    # Calculate summary information
+    summary_info = calculate_summary()
 
-    data_text.delete("1.0", "end")
-    data_text.insert("1.0", df.to_string())
+    # Display the data summary in the GUI
+    display_summary(summary_info)
+
+    # Update the log widget
+    log_text.insert(tk.END, "Data fetched successfully.\n")
 
     print(df)
 
     paste_button.config(state=tk.NORMAL)
+
+def calculate_summary():
+    global dataframe
+
+    # Calculate the number of rows
+    num_rows = len(dataframe)
+
+    # Calculate the sums of "CONV AMT" based on "sch A acc" code
+    sums_by_sch_a_code = dataframe.groupby("sch A acc")["CONV  AMT"].sum().reset_index()
+
+    # Store the summary information
+    summary_info = {
+        "Number of Rows": num_rows,
+        "Sums by sch A acc": sums_by_sch_a_code
+    }
+
+    return summary_info
+
+def display_summary(summary_info):
+    # Display the data summary in the GUI
+    data_text.delete("1.0", "end")
+    data_text.insert("1.0", "Summary Information:\n\n")
+
+    # Display the number of rows
+    num_rows = summary_info["Number of Rows"]
+    data_text.insert(tk.END, f"Number of Rows: {num_rows}\n\n")
+
+    # Display the sums by sch A acc
+    sums_by_sch_a_acc = summary_info["Sums by sch A acc"]
+    sums_by_group = sums_by_sch_a_acc.groupby(sums_by_sch_a_acc["sch A acc"].str[:4])
+    data_text.insert(tk.END, "Sums by Group:\n")
+    for group, group_data in sums_by_group:
+        conv_amt_sum = abs(group_data["CONV  AMT"].sum())
+        data_text.insert(tk.END, f"Group: {group}\tCONV AMT: {conv_amt_sum:,.2f}\n")
+
+    # Display the sums by sch A acc (2 digits)
+    sums_by_group_2 = sums_by_sch_a_acc.groupby(sums_by_sch_a_acc["sch A acc"].str[:2])
+    data_text.insert(tk.END, "\nSums by Group:\n")
+    for group_2, group_data_2 in sums_by_group_2:
+        conv_amt_sum_2 = abs(group_data_2["CONV  AMT"].sum())
+        data_text.insert(tk.END, f"Group: {group_2}\tCONV AMT: {conv_amt_sum_2:,.2f}\n")
+
+    # Display the sum of all data
+    conv_amt_sum_total = abs(sums_by_sch_a_acc["CONV  AMT"].sum())
+    data_text.insert(tk.END, f"\nTotal CONV AMT: {conv_amt_sum_total:,.2f}\n")
 
 def paste_data():
     global dataframe
@@ -86,7 +136,7 @@ def paste_data():
     # Get the "ALL CP" sheet from the workbook
     cp_sheet = wb_to_use.sheets["DataExtraction"]
 
-    # Find the last row with data in column A
+    # Find the last row with data in column A and clear the content from the sheet
     last_row_cp = cp_sheet.range("A2:C" + str(cp_sheet.cells.last_cell.row))
     last_row_cp.clear_contents()
 
@@ -95,6 +145,9 @@ def paste_data():
     dest_range = cp_sheet.range("A2:C{}".format(len(dataframe) + 1))
 
     dest_range.value = dataframe[["ACCT NO.", "sch A acc","CONV  AMT"]].values
+
+    # Update the log widget
+    log_text.insert(tk.END, "Data pasted successfully.\n")
 
     print("Data pasted successfully.")
 
@@ -109,16 +162,20 @@ file_entry = tk.Entry(root)
 file_entry.pack()
 
 # Text widget for displaying data
-data_text = Text(root)
+data_text = Text(root, width=80,height=20)
 data_text.pack()
 
 # Button to fetch data
 fetch_button = Button(root, text="Fetch Data", command=fetch_data)
 fetch_button.pack()
 
-#Button to paste data
+# Button to paste data
 paste_button = Button(root, text="Paste Data in ALL CP tab", command=paste_data, state=tk.DISABLED)
 paste_button.pack()
+
+# Log widget
+log_text = Text(root, height=5)
+log_text.pack()
 
 # Run the GUI
 root.mainloop()
